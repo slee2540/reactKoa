16826e4c349c36de75e9c2729ad5bbaa344fd1c3
slee2540 <slee2540@gmail.com>
2019-04-04 14:50:51 +0900

    index 수정

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
 import * as serviceWorker from './serviceWorker';
-import configureStore from 'redux/configureStore';
+import configureStore from './redux/configureStore';
 
 const store = configureStore();
 
